1526859241ca8dca5ee2b155899a4eba43ec0659
te6lim <te6lim@gmail.com>
2024-11-08 12:11:00 +0100

    define controller functions

---
+++ controllers/TodoController.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+func GetAllTodos(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func GetTodo(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func CreateTodo(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func UpdateTodo(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func DeleteTodo(responseWriter http.ResponseWriter, request *http.Request) {
+	
+}
