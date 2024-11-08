a939069532027265b2a00a0603321eefb74afb16
te6lim <te6lim@gmail.com>
2024-11-08 12:52:15 +0100

    define note controller functions

---
+++ controllers/note-controller.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+func CreateNote(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func UpdateNote(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func DeleteNote(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func GetNote(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
+
+func GetAllNotes(responseWriter http.ResponseWriter, request *http.Request) {
+
+}
