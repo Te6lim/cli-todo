32ca702b7ec3969cb600394d162a1f97ee025a1a
te6lim <te6lim@gmail.com>
2024-11-09 15:44:44 +0100

    implement CreateTodo end-point

--- controllers/todo-controller.go
+++ controllers/todo-controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -10,6 +11,7 @@ import (
 	"github.com/te6lim/whattodo/controllers/params"
 	"github.com/te6lim/whattodo/database"
 	"github.com/te6lim/whattodo/database/interfaces"
+	"github.com/te6lim/whattodo/models"
 )
 
 var todoDatabase interfaces.TodoStorage = &database.TodoDb{}
@@ -22,15 +24,16 @@ func GetTodo(responseWriter http.ResponseWriter, request *http.Request) {
 	parameters := mux.Vars(request)
 	todoId, err := strconv.ParseInt(parameters[params.TodoId], 0, 0)
 	if err != nil {
-		fmt.Println("error while parsing")
+		fmt.Println("unable to parse json")
 	}
 	todo, err := todoDatabase.GetTodo(todoId)
 	if err != nil {
 		responseWriter.WriteHeader(http.StatusNotFound)
+		return
 	}
-	res, _ := json.Marshal(todo)
+	res, err := json.Marshal(todo)
 	if err != nil {
-		fmt.Println("unable to convert json")
+		fmt.Println("unable to convert todo object to json")
 	}
 	responseWriter.Header().Set("Content-Type", "application/json")
 	responseWriter.WriteHeader(http.StatusOK)
@@ -38,7 +41,29 @@ func GetTodo(responseWriter http.ResponseWriter, request *http.Request) {
 }
 
 func CreateTodo(responseWriter http.ResponseWriter, request *http.Request) {
+	todo := &models.Todo{}
 
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		fmt.Println("error reading request body")
+	}
+
+	if err := json.Unmarshal(body, todo); err != nil {
+		fmt.Println("unable to convert json to todo object")
+	}
+
+	todoDatabase.InsertTodo(*todo)
+
+	res, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("unable to convert todo object ot json")
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	responseWriter.Header().Set("Content-Type", "application/json")
+	responseWriter.WriteHeader(http.StatusOK)
+	responseWriter.Write(res)
 }
 
 func UpdateTodo(responseWriter http.ResponseWriter, request *http.Request) {
--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/te6lim/whattodo/routes"
 )
 
 func main() {
 	fmt.Println("here we go again...")
+	router := mux.NewRouter()
+	routes.RegisterTodoRoutes(router)
+	http.Handle("/", router)
+
+	fmt.Println("listening on port:2020")
+	if err := http.ListenAndServe("localhost:2020", router); err != nil {
+		log.Fatal("failed to connect to port:2020")
+	}
 }
